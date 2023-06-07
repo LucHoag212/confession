c842a4a040cf30e9fd04f638eb68370d73d84037
LucHoag212 <125335096+LucHoag212@users.noreply.github.com>
2023-06-08 01:42:10 +0700

    Link cấm của Hào đbrr

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'zân zân🤍';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'hép pi bớt đây';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Khong biết chúc cậu như thế nào nhưng mà chúc cậu tuổi mới không phải buồn và suy nghĩ về chuyện gì cả, mong cậu luôn mỉm cười hạnh phúc ở tuổi mới, có những thứ cậu muốn và làm được những điều cậu muốn làm. Nghiêm khắc với bản thân, khoan dung với người khác nhé🤍 <br> Thêm 1 tuổi mới càng phải tự tin vào bản thân, tươi sáng, tự tin và kiên trì nhé. Đặc biệt thức khuya ít thôi nhé đi ngủ sớm hơn🤜 <br> Mình chỉ biết chúc như vậy và mình muốn nói ờ thì tớ có tình cảm với cậu và mình không biết yêu cậu từ lúc nào nữa. Chắc chắn là mình đã yêu cậu dồi và mình muốn nói lời yêu lời tỏ tình với cậu rất lâu dồi cũng rất nhiều lần mình muốn nói ra nhưng lúc đấy mình lại nghĩ cậu chưa muốn yêu và chưa đến lúc nên mình không dám nói. Và điều gì đến thì cũng sẽ đến trong ngày sinh nhật tuổi mới của cậu mình muốn nói là "cậu có thể làm người yêu mình" được khong☺️.Dù có hơi bất ngờ một xíu và có như nào thì mình vẫn muốn nghe câu trả lời từ cậu😳.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'ava.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
